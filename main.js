ba2a419b301bec32b8aef2a0c603f6e4d4e65998
gfernan24373 <gilberto_fernandez_100@hotmail.com>
2022-04-17 22:00:28 -0400

    upgrading responsive design and adding trasitioning style when the answer are displayed in the screen by de button.

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-let button = document.querySelectorAll('.faq-button');
-let faq = document.querySelectorAll('.p-faq');
+let btn = document.querySelectorAll('.btn');
+let respuesta = document.querySelectorAll('.respuesta');
 let icon = document.querySelectorAll('.icon');
-//first loop to set property display = none to all faqs answers (<p>)
-for(faqs of faq) {
-	faqs.style.display = 'none';
-}
 
-//settings all the selected elements
-button.forEach((v, i, arr) => {
-	arr[i].onclick = () =>{
-		if(faq[i].style.display == 'none') {
-			faq[i].style.display = 'block';
-			arr[i].style.fontWeight = 'bold';
-			icon[i].style.transform = 'rotate(0.5turn)';
-		} else {
-			faq[i].style.display = 'none';
+btn.forEach((v, i, arr) => {
+	arr[i].onclick = () => {
+		if(arr[i].nextElementSibling.style.maxHeight) { //Look the sibling of button(p tag) and if max-height != 0 set maxHeight to 0 again.
+			arr[i].nextElementSibling.style.maxHeight = null;
 			arr[i].style.fontWeight = 'normal';
 			icon[i].style.transform = 'rotate(1turn)'; //rotate the arrow icon when the user click on button
+		} else {
+			arr[i].nextElementSibling.style.maxHeight = arr[i].nextElementSibling.scrollHeight + 'px';
+			arr[i].style.fontWeight = 'bold';
+			icon[i].style.transform = 'rotate(0.5turn)';
 		}
 	}
 });
